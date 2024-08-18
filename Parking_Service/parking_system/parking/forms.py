1508c5627c17f487166f5f418120073ca393998f
pentagon846 <pentagon846@gmail.com>
2024-08-18 22:02:06 +0200

    fix form registration

--- Parking_Service/parking_system/parking/forms.py
+++ Parking_Service/parking_system/parking/forms.py
@@ -10,8 +10,27 @@ class ParkingImageForm(forms.ModelForm):
         fields = ['image']
 
 
+# class UserRegisterForm(UserCreationForm):
+#     email = forms.EmailField()
+#
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'password1', 'password2']
+
+
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    username = forms.CharField(max_length=50,
+                               required=True,
+                               widget=forms.TextInput(attrs={"class": "form-control"}))
+    email = forms.EmailField(required=True,
+                             widget=forms.EmailInput(attrs={"class": "form-control"}))
+
+    password1 = forms.CharField(max_length=150,
+                                required=True,
+                                widget=forms.PasswordInput(attrs={"class": "form-control"}))
+    password2 = forms.CharField(max_length=150,
+                                required=True,
+                                widget=forms.PasswordInput(attrs={"class": "form-control"}))
 
     class Meta:
         model = User
@@ -22,5 +41,3 @@ class VehicleForm(forms.ModelForm):
     class Meta:
         model = Vehicle
         fields = ['license_plate']
-
-
