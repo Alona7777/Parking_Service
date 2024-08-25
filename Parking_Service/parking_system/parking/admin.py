a4adc14c75c64512c0d298c0cbf510600566f451
Artem Naboka <83280960+SkyFoxNa@users.noreply.github.com>
2024-08-25 17:01:51 +0300

    Add transaction

--- Parking_Service/parking_system/parking/admin.py
+++ Parking_Service/parking_system/parking/admin.py
@@ -1,13 +1,13 @@
 from django import forms
 from django.contrib import admin
-from .models import Vehicle, ParkingSession, ParkingRate, ParkingSpot
+from .models import Vehicle, ParkingSession, ParkingRate, ParkingSpot, UserProfile, Transaction
 
 @admin.register(Vehicle)
 class VehicleAdmin(admin.ModelAdmin):
     list_display = ('license_plate', 'owner', 'vehicle_type', 'is_blocked', 'parking_spot', 'subscription_end_date', 'is_disabled')
     list_filter = ('vehicle_type', 'is_blocked')
     search_fields = ('license_plate', 'owner__username')
-    list_editable = ('is_blocked',)
+    list_editable = ('is_blocked', 'parking_spot', 'subscription_end_date', 'is_disabled')
 
 @admin.register(ParkingSpot)
 class ParkingSpotAdmin(admin.ModelAdmin):
@@ -20,4 +20,14 @@ class ParkingSessionAdmin(admin.ModelAdmin):
 
 @admin.register(ParkingRate)
 class ParkingRateAdmin(admin.ModelAdmin):
-    list_display = ('vehicle_type', 'rate_per_hour')
+    list_display = ('vehicle_type', 'rental_rate', 'rate_per_hour', 'disabled_rate')
+    list_editable = ('rental_rate', 'rate_per_hour', 'disabled_rate')
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'monetary_limit']
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'transaction_type', 'amount', 'timestamp', 'description')
+    search_fields = ('user__username', 'transaction_type')
